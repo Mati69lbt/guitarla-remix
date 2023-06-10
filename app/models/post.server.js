d15062b10bfaf195d8ea054c6febf26af5b3f790
Mati69lbt <Mati69_lbt@hotmail.com>
2023-06-10 18:09:11 -0300

    blog terminado

--- app/models/post.server.js
+++ app/models/post.server.js
@@ -3,3 +3,12 @@ export async function getPosts() {
   const respuesta = await fetch(`${url}/posts?populate=imagen`);
   return await respuesta.json();
 }
+
+export async function getPost(url) {
+  const urlApi = process.env.API_URL;
+  const respuesta = await fetch(
+    `${urlApi}/posts?filters[url]=${url}&populate=imagen`
+  );
+
+  return await respuesta.json();
+}
---
+++ app/routes/posts.$postUrl.jsx
@@ -0,0 +1,62 @@
+import { useLoaderData } from "@remix-run/react";
+import { getPost } from "~/models/post.server";
+import { formatearFecha } from "~/utils/helpers";
+import style from "../styles/blog.css";
+
+export async function loader({ params }) {
+  const { postUrl } = params;
+  const post = await getPost(postUrl);
+  if (post.data.length === 0) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Entrada no encontrada",
+    });
+  }
+  return post;
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: style,
+    },
+  ];
+}
+
+export function meta({ data }) {
+  if (!data) {
+    return [
+      { title: "GuitarLa - Pag no encontrada" },
+      {
+        descrpcion:
+          "Venta de Guitarras, Blog de Musica, Coleccion de guitarras, guitarras baratas",
+      },
+    ];
+  }
+  const titulo = data?.data[0]?.attributes.titulo;
+
+  return [
+    { title: `GuitarLA - ${titulo}` },
+    {
+      descrpcion:
+        "Venta de Guitarras, Blog de Musica, Coleccion de guitarras, guitarras baratas",
+    },
+  ];
+}
+
+export default function PostUrl() {
+  const post = useLoaderData();
+  const { titulo, contenido, imagen, publishedAt } = post?.data[0].attributes;
+  const img = imagen?.data?.attributes.formats.medium.url;
+  return (
+    <article className="post">
+      <img className="imagen" src={img} alt={titulo} />
+      <div className="contenido">
+        <h3>{titulo}</h3>
+        <p className="fecha">{formatearFecha(publishedAt)}</p>
+        <p className="texto">{contenido}</p>
+      </div>
+    </article>
+  );
+}
